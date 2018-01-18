3986562370452ad9d933e3507d9143049400f671
Ignatiy <ignatiy.anatolich@mail.ru>
2018-01-18 12:50:59 +0000

    Chapter 7 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    #@user = User.new(params[:user]).permit(:name, :email, :password, :password_confirmation)
+    @user = User.new(user_params)
+    flash[:success] = "Welcome to the Sample App Created by Ignatiy!Dobro pozalovat gosti dorogie :)"
+    if @user.save
+      redirect_to @user
+      #redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
 end
