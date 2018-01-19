32e2548f02035699e355f892abd25532f1872ea0
Ignatiy <ignatiy.anatolich@mail.ru>
2018-01-19 14:27:32 +0000

    Updated

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -8,18 +8,12 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   end
   
   test "index including pagination" do
-    log_in_as(@user)
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
-      end
-    end
-    assert_difference 'User.count', -1 do
-      delete user_path(@non_admin)
     end
     assert_select 'div.pagination', count: 2
   end
